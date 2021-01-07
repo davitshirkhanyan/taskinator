347fb819c382350387de33624a479200e5a5468d
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-06 23:08:46 -0800

    add drag-drop functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,52 @@ var taskStatusChangeHandler = function(event) {
     }
 };
 
+var dragTaskHandler = function(event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+
+};
+
+var dropZoneDraghandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+
+    if (taskListEl) {
+        event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+    }
+};
+
+var dropTaskHandler = function(event) {
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+
+    // set status of task based on dropZone id
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    
+    if (statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    }
+    else if (statusType === "tasks-in-progress") {
+        statusSelectEl.selectedIndex = 1;
+    }
+    else if (statusType === "tasks-completed") {
+        statusSelectEl.selectedIndex = 2;
+    }
+    dropZoneEl.removeAttribute("style");
+    dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+};
+
 var completeEditTask = function(taskName, taskType, taskId) {
     // find the matching task list item
     // find the matching task list item
@@ -95,6 +141,7 @@ listItemEl.className = "task-item";
 
 // add task id as a custom attribute
 listItemEl.setAttribute("data-task-id", taskIdCounter);
+listItemEl.setAttribute("draggable", "true");
 
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
@@ -181,3 +228,7 @@ var deleteTask = function(taskId) {
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
+pageContentEl.addEventListener("dragover", dropZoneDraghandler);
+pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
